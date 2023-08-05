a772621555ecd9124b6b7427f3bc2115b7db0b71
Jacky Huang <lashhw81@gmail.com>
2023-08-05 16:04:48 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <vector>
+#include <bvh/triangle.hpp>
+#include <bvh/bvh.hpp>
+#include <bvh/sweep_sah_builder.hpp>
+#include "happly/happly.h"
+
+typedef bvh::Bvh<float> bvh_t;
+typedef bvh::Triangle<float> triangle_t;
+typedef bvh::Vector3<float> vector_t;
+typedef bvh::BoundingBox<float> bbox_t;
+typedef bvh::SweepSahBuilder<bvh_t> builder_t;
+typedef bvh_t::Node node_t;
+
+bbox_t get_quant_bbox(bvh_t &bvh, size_t node_idx, size_t quant_idx, int quant_bits) {
+    bbox_t node_bbox = bvh.nodes[node_idx].bounding_box_proxy().to_bounding_box();
+    bbox_t quant_bbox = bvh.nodes[quant_idx].bounding_box_proxy().to_bounding_box();
+    int quant_num = 1 << quant_bits;
+
+    bbox_t ret;
+    for (int i = 0; i < 3; i++) {
+        float scaling_factor = (quant_bbox.max[i] - quant_bbox.min[i]) / (float)quant_num;
+        ret.min[i] = quant_bbox.min[i] + scaling_factor * floorf((node_bbox.min[i] - quant_bbox.min[i]) / scaling_factor);
+        ret.max[i] = quant_bbox.min[i] + scaling_factor * ceilf((node_bbox.max[i] - quant_bbox.min[i]) / scaling_factor);
+    }
+    return ret;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        std::cerr << "usage: ./a.out MODEL_FILE QUANT_BITS T_TRV T_SWITCH T_IST" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    char *model_file = argv[1];
+    int quant_bits = std::stoi(argv[2]);
+    float t_trv = std::stof(argv[3]);
+    float t_switch = std::stof(argv[4]);
+    float t_ist = std::stof(argv[5]);
+    std::cout << "MODEL_FILE = " << model_file << std::endl;
+    std::cout << "QUANT_BITS = " << quant_bits << std::endl;
+    std::cout << "T_TRV = " << t_trv << std::endl;
+    std::cout << "T_SWITCH = " << t_switch << std::endl;
+    std::cout << "T_IST = " << t_ist << std::endl;
+
+    happly::PLYData ply_data(model_file);
+    std::vector<std::array<double, 3>> v_pos = ply_data.getVertexPositions();
+    std::vector<std::vector<size_t>> f_idx = ply_data.getFaceIndices<size_t>();
+
+    std::vector<triangle_t> triangles;
+    for (auto &face : f_idx) {
+        triangles.emplace_back(
+            vector_t((float)v_pos[face[0]][0], (float)v_pos[face[0]][1], (float)v_pos[face[0]][2]),
+            vector_t((float)v_pos[face[1]][0], (float)v_pos[face[1]][1], (float)v_pos[face[1]][2]),
+            vector_t((float)v_pos[face[2]][0], (float)v_pos[face[2]][1], (float)v_pos[face[2]][2])
+        );
+    }
+
+    auto [bboxes, centers] = bvh::compute_bounding_boxes_and_centers(triangles.data(), triangles.size());
+    auto global_bbox = bvh::compute_bounding_boxes_union(bboxes.get(), triangles.size());
+    std::cout << "global_bbox = ("
+              << global_bbox.min[0] << ", " << global_bbox.min[1] << ", " << global_bbox.min[2] << "), ("
+              << global_bbox.max[0] << ", " << global_bbox.max[1] << ", " << global_bbox.max[2] << ")" << std::endl;
+
+    std::cout << "building..." << std::endl;
+    bvh_t bvh;
+    builder_t builder(bvh);
+    builder.build(global_bbox, bboxes.get(), centers.get(), triangles.size());
+
+    int t_buf_size = 0;
+    std::stack<std::pair<size_t, int>> stk_1;
+    std::vector<int> t_buf_map(bvh.node_count, -1);
+    node_t &root_node = bvh.nodes[0];
+    size_t root_left_node_idx = root_node.first_child_or_primitive;
+    size_t root_right_node_idx = root_left_node_idx + 1;
+    stk_1.emplace(root_right_node_idx, 1);
+    stk_1.emplace(root_left_node_idx, 1);
+    while (!stk_1.empty()) {
+        auto [curr_idx, depth] = stk_1.top();
+        node_t &curr_node = bvh.nodes[curr_idx];
+        stk_1.pop();
+
+        t_buf_map[curr_idx] = t_buf_size;
+        t_buf_size += depth;
+
+        if (!curr_node.is_leaf()) {
+            size_t left_node_idx = curr_node.first_child_or_primitive;
+            size_t right_node_idx = left_node_idx + 1;
+            stk_1.emplace(left_node_idx, depth + 1);
+            stk_1.emplace(right_node_idx, depth + 1);
+        }
+    }
+
+    std::vector<size_t> parent(bvh.node_count);
+    std::vector<float> t_buf(t_buf_size);
+    std::vector<bool> stay(t_buf_size);
+    std::stack<std::pair<size_t, bool>> stk_2;
+    stk_2.emplace(root_right_node_idx, true);
+    stk_2.emplace(root_left_node_idx, true);
+    while (!stk_2.empty()) {
+        auto [curr_idx, first] = stk_2.top();
+        node_t &curr_node = bvh.nodes[curr_idx];
+        stk_2.pop();
+
+        size_t left_node_idx = curr_node.first_child_or_primitive;
+        size_t right_node_idx = left_node_idx + 1;
+
+        if (first) {
+            stk_2.emplace(curr_idx, false);
+            if (!curr_node.is_leaf()) {
+                stk_2.emplace(right_node_idx, true);
+                stk_2.emplace(left_node_idx, true);
+                parent[left_node_idx] = curr_idx;
+                parent[right_node_idx] = curr_idx;
+            }
+        } else {
+            size_t quant_idx = parent[curr_idx];
+            for (int i = 0; ; i++) {
+                bbox_t quant_bbox = get_quant_bbox(bvh, curr_idx, quant_idx, quant_bits);
+                float half_area = quant_bbox.half_area();
+
+                float &curr_t_buf = t_buf[t_buf_map[curr_idx] + i];
+                if (curr_node.is_leaf()) {
+                    curr_t_buf = t_ist * (float)curr_node.primitive_count * half_area;
+                } else {
+                    float left_stay_t = t_buf[t_buf_map[left_node_idx] + 1 + i];
+                    float right_stay_t = t_buf[t_buf_map[right_node_idx] + 1 + i];
+                    float curr_stay_t = t_trv * 2 * half_area + left_stay_t + right_stay_t;
+
+                    float left_switch_t = t_buf[t_buf_map[left_node_idx]];
+                    float right_switch_t = t_buf[t_buf_map[right_node_idx]];
+                    float curr_switch_t = (t_trv * 2 + t_switch) * half_area + left_switch_t + right_switch_t;
+
+                    if (curr_stay_t < curr_switch_t) {
+                        t_buf[curr_idx] = curr_stay_t;
+                        stay[curr_idx] = true;
+                    } else {
+                        t_buf[curr_idx] = curr_switch_t;
+                        stay[curr_idx] = false;
+                    }
+                }
+
+                if (quant_idx == 0)
+                    break;
+                else
+                    quant_idx = parent[quant_idx];
+            }
+        }
+    }
+}
