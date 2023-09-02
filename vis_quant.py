e4afbcaa499325cd45392c9175e80a75188df5df
Jacky Huang <lashhw81@gmail.com>
2023-09-02 22:58:31 +0800

    .

--- vis_quant.py
+++ vis_quant.py
@@ -2,60 +2,50 @@ import hashlib
 import numpy as np
 import pyvista as pv
 import matplotlib.pyplot as plt
+import ctypes
 from matplotlib.colors import ListedColormap
 from tqdm import tqdm
 
-num_samples = 2048
-num_colors = 20
+NUM_SAMPLES = 2048
+NUM_COLORS = 20
 
 print('creating grid...')
-u = np.linspace(0, 2 * np.pi, num_samples)
-v = np.linspace(0, np.pi, num_samples)
+u = np.linspace(0, 2 * np.pi, NUM_SAMPLES)
+v = np.linspace(0, np.pi, NUM_SAMPLES)
 x = np.outer(np.cos(u), np.sin(v))
 y = np.outer(np.sin(u), np.sin(v))
 z = np.outer(np.ones(np.size(u)), np.cos(v))
 grid = pv.StructuredGrid(x, y, z)
 del u, v, x, y, z
 
-def summary(a, b, c):
-    def transform(x):
-        sign = 1 if x & (1 << 31) else 0
-        exponent = (x >> 23) & 0b011111111
-        mantissa = x & ((1 << 23) - 1)
-
-        new_exponent = (exponent - (127 + 7)) & 0b11111
-        new_mantissa = 0b10000000 | (mantissa >> 16)
-
-        return ((sign << 13) | (new_exponent << 8) | new_mantissa), (new_exponent & 1)
-
-    ta, ea = transform(a)
-    tb, eb = transform(b)
-    tc, ec = transform(c)
-    encoded = (ta << 28) | (tb << 14) | tc
-    return encoded, (ea ^ eb ^ ec)
-
-def hash_to_color(encoded, quant_type):
-    x_bytes = int(encoded).to_bytes(6, 'big')
-    h = hashlib.sha256(x_bytes).digest()
-    c = int.from_bytes(h, 'big') % num_colors
-    if quant_type:
-        c += num_colors
-    return c
-
-print('computing color...')
-cx = np.empty(grid.n_cells, dtype=np.float32)
-cy = np.empty(grid.n_cells, dtype=np.float32)
-cz = np.empty(grid.n_cells, dtype=np.float32)
+print('extracting center...')
+cx = np.empty(grid.n_cells, dtype=np.single)
+cy = np.empty(grid.n_cells, dtype=np.single)
+cz = np.empty(grid.n_cells, dtype=np.single)
 for i in tqdm(range(grid.n_cells)):
     cx[i], cy[i], cz[i] = grid.get_cell(i).center
+
+print('calculating inverse...')
 sw = 0.0078125  # 2^(-7)
-ix = np.floor(1.0 / (cx * sw)).view(np.uint32)
-iy = np.floor(1.0 / (cy * sw)).view(np.uint32)
-iz = np.floor(1.0 / (cz * sw)).view(np.uint32)
-grid.cell_data['color'] = np.empty(grid.n_cells, dtype=int)
-for i in tqdm(range(grid.n_cells)):
-    encoded, quant_type = summary(ix[i], iy[i], iz[i])
-    grid.cell_data['color'][i] = hash_to_color(encoded, quant_type)
+ix = np.floor(1.0 / (cx * sw)).view(np.uintc)
+iy = np.floor(1.0 / (cy * sw)).view(np.uintc)
+iz = np.floor(1.0 / (cz * sw)).view(np.uintc)
+
+print('calculating color...')
+ixp = ix.ctypes.data_as(ctypes.POINTER(ctypes.c_uint))
+iyp = iy.ctypes.data_as(ctypes.POINTER(ctypes.c_uint))
+izp = iz.ctypes.data_as(ctypes.POINTER(ctypes.c_uint))
+cp = (ctypes.c_uint * grid.n_cells)()
+encode_and_hash = ctypes.CDLL('/tmp/encode_and_hash.so').encode_and_hash
+encode_and_hash.argtypes = [
+    ctypes.c_int,
+    ctypes.POINTER(ctypes.c_uint),
+    ctypes.POINTER(ctypes.c_uint),
+    ctypes.POINTER(ctypes.c_uint),
+    ctypes.POINTER(ctypes.c_uint)
+]
+encode_and_hash(grid.n_cells, ixp, iyp, izp, cp)
+grid.cell_data['color'] = cp
 del cx, cy, cz, ix, iy, iz
 
 print('plotting...')
