671d36741daf529175f56fb88322d6440ba1b71b
Jacky Huang <lashhw81@gmail.com>
2023-09-02 18:03:18 +0800

    .

--- vis_quant.py
+++ vis_quant.py
@@ -3,6 +3,7 @@ import numpy as np
 import pyvista as pv
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from tqdm import tqdm
 
 num_samples = 2048
 num_colors = 20
@@ -14,6 +15,7 @@ x = np.outer(np.cos(u), np.sin(v))
 y = np.outer(np.sin(u), np.sin(v))
 z = np.outer(np.ones(np.size(u)), np.cos(v))
 grid = pv.StructuredGrid(x, y, z)
+del u, v, x, y, z
 
 def summary(a, b, c):
     def transform(x):
@@ -44,17 +46,17 @@ print('computing color...')
 cx = np.empty(grid.n_cells, dtype=np.float32)
 cy = np.empty(grid.n_cells, dtype=np.float32)
 cz = np.empty(grid.n_cells, dtype=np.float32)
-for i in range(grid.n_cells):
+for i in tqdm(range(grid.n_cells)):
     cx[i], cy[i], cz[i] = grid.get_cell(i).center
 sw = 0.0078125  # 2^(-7)
 ix = np.floor(1.0 / (cx * sw)).view(np.uint32)
 iy = np.floor(1.0 / (cy * sw)).view(np.uint32)
 iz = np.floor(1.0 / (cz * sw)).view(np.uint32)
-colors = np.empty(grid.n_cells, dtype=int)
-for i in range(grid.n_cells):
+grid.cell_data['color'] = np.empty(grid.n_cells, dtype=int)
+for i in tqdm(range(grid.n_cells)):
     encoded, quant_type = summary(ix[i], iy[i], iz[i])
-    colors[i] = hash_to_color(encoded, quant_type)
-grid.cell_data['color'] = colors
+    grid.cell_data['color'][i] = hash_to_color(encoded, quant_type)
+del cx, cy, cz, ix, iy, iz
 
 print('plotting...')
 p = pv.Plotter()
