7d71806598855048cd85823bc25e09fc89d9f3ce
Jacky Huang <lashhw81@gmail.com>
2023-09-23 14:38:33 +0800

    .

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ int main(int argc, char *argv[]) {
     arg_t arg = parse_arg(argc, argv);
 
     std::cout << "loading..." << std::endl;
-    std::vector<triangle_t> triangles = load_triangles(arg.model_file);
+    std::vector<trig_t> trigs = load_trigs(arg.model_file);
 
     std::cout << "building..." << std::endl;
-    bvh_t bvh = build_bvh(triangles);
+    bvh_t bvh = build_bvh(trigs);
 
     std::cout << "clustering..." << std::endl;
-    int_bvh_t int_bvh = build_int_bvh(arg.t_trv_int, arg.t_switch, arg.t_ist, bvh);
+    int_bvh_t int_bvh = build_int_bvh(arg.t_trv_int, arg.t_switch, arg.t_ist, trigs, bvh);
 }
